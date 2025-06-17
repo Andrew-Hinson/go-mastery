42a2b880a24cb85e379cd95cd9187797ae0738d2
andrew hinson <a.hinson@fetchrewards.com>
2025-06-17 05:51:47 -0500

    first stab

--- leetcode/maxConsecutiveOnesIII/solution.go
+++ leetcode/maxConsecutiveOnesIII/solution.go
@@ -7,3 +7,29 @@ package maxConsecutiveOnesIII
 //Output: 6
 //Explanation: [1,1,1,0,0,1,1,1,1,1,1]
 //Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+// basically, what is the greatest number between right and left
+func findMaxConsecutiveOnes(ones []int, k int) int {
+	left := 0
+	ans := 0
+	//current is the number of 0s in the window
+	curr := 0
+	for right := range ones {
+		if ones[right] == 0 {
+			curr += 1
+		}
+		//while right has added more than 2 zeros to curr, bring left along with it till it gets to 0s and subtract from curr
+		for curr > 2 {
+			if ones[left] == 0 {
+				curr -= 1
+			}
+			ones[left] += 1
+		}
+		if ans > right-left+1 {
+			ans = 0
+		} else {
+			ans = right - left + 1
+		}
+	}
+	return ans
+}
