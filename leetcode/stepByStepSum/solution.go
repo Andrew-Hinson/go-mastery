dc13a32db369949e7bfb86ec30992b6cc0a7ebf0
andrew hinson <a.hinson@fetchrewards.com>
2025-06-29 11:16:12 -0500

    init new problem

---
+++ leetcode/stepByStepSum/solution.go
@@ -0,0 +1,13 @@
+package stepByStepSum
+
+//Given an array of integers nums, you start with an initial positive value startValue.
+//In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+//Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+
+//Input: nums = [-3,2,-3,4,2]
+//Output: 5
+//Explanation: If you choose startValue = 4, in the third iteration your step by step sum is less than 1.
+
+func minStartValue(nums []int) int {
+
+}
