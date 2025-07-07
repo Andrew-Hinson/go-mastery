1bcbd624d5ab2760ed344eaa6b6a455dc84d6573
andrew hinson <a.hinson@fetchrewards.com>
2025-07-07 07:40:06 -0500

    first stab

--- leetcode/kRadiusAverages/solution.go
+++ leetcode/kRadiusAverages/solution.go
@@ -1,5 +1,7 @@
 package kRadiusAverages
 
+import "fmt"
+
 // You are given a 0-indexed array nums of n integers, and an integer k.
 // The k-radius average for a subarray of nums centered at some index i
 // with the radius k is the average of all elements in nums between the indices i - k
@@ -14,6 +16,31 @@ package kRadiusAverages
 // Input: nums = [7,4,3,9,1,8,5,2,6], k = 3
 // Output: [-1,-1,-1,5,4,4,-1,-1,-1]
 
-func getAverages(nums []int, k int) []int {
-	return nums
+func GetAverages(nums []int, k int) []int {
+	//holds the subarray total
+	totals := []int{nums[0]}
+	averages := []int{}
+	for i := 1; i < len(nums); i++ {
+		//nums[i]+averages[len(averages)-1] = last element of averages
+		totals = append(totals, nums[i]+totals[len(totals)-1])
+	}
+	//I think I iterate over totals and compute the averages with k radius
+	for j := 0; j < len(totals); j++ {
+		//if j is less than k, j equals -1.
+		//if the len(total) - k, j equals -1
+		if j < k {
+			averages = append(averages, -1)
+		}
+		if j >= k && j <= len(totals)-k {
+			//	sum exists in totals, to get total to average, get last element +k - nums[k*2]
+			averages = append(averages, totals[j+k]/k*2)
+		}
+		if j > k {
+			averages = append(averages, -1)
+		}
+
+	}
+	fmt.Println(averages)
+
+	return averages
 }
