f0781be0f3d88a52cee5d48082d3d829f623d23a
andrew hinson <a.hinson@fetchrewards.com>
2025-07-17 12:11:44 -0500

    ooooffff off by 1, im close I think

--- leetcode/subArrayProduct/solution.go
+++ leetcode/subArrayProduct/solution.go
@@ -1,6 +1,9 @@
 package subArrayProduct
 
-//Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+import "fmt"
+
+//Given an array of integers nums and an integer k,
+//return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
 
 //Example 1:
 //Input: nums = [10,5,2,6], k = 100
@@ -13,7 +16,35 @@ package subArrayProduct
 //Input: nums = [1,2,3], k = 0
 //Output: 0
 
+//Input: nums = [10,5,2,6], k = 100
+//Output: 8
+
 func NumSubarrayProductLessThanK(nums []int, k int) int {
-	myInt := 0
-	return myInt
+	//I need to build a product array maybe?
+	//sliding window problem methinks
+	// I need to iterate through nums,
+	// If the number is < k, add it to the product array
+	//multiply nums together, for each successful multiply that's less than 100, I increment a counter.
+	n := len(nums)
+	validNums := make([]int, n)
+	left := 0
+	right := 0
+	counter := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < k {
+			counter++
+			validNums[i] = nums[i]
+		}
+	}
+	for j := 0; j < len(validNums); j++ {
+		if validNums[left]*validNums[right] < k {
+			right++
+			counter++
+		}
+		left++
+	}
+	fmt.Println(counter)
+
+	return k
 }
